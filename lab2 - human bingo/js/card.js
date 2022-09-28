2ca0d75562d8ab58aed5a1fffcd6604c3cca863e
Ismail El Ghamri <elghamri.ismail@gmail.com>
2022-09-28 10:25:58 +0200

    feat: calling bingo functions in cards

--- lab2 - human bingo/js/card.js
+++ lab2 - human bingo/js/card.js
@@ -23,17 +23,21 @@ export default class Card {
     let card = document.createElement("div");
     card.classList.add("bingo__card");
     card.setAttribute("data-card", counter);
+    card.dataset.number = counter + 1;
     card.innerHTML = this.title;
     document.querySelector(".bingo__board").appendChild(card);
-
-    // card.dataset.number = counter + 1;
+ 
+   
     // don't forget to append the child to to DOM
 
     // 🔥🔥🔥 TODO4: when we click an item, we want to check for winners and we want to save the selection to storage
     card.addEventListener("click", (e) => {
-      // this.markDone(e.target);
+      this.markDone(e.target);
       // call checkWinner() on the Bingo class
       // try to call the save() method on the Bingo class
+       Bingo.checkWinner();
+       Bingo.save();
+         
     });
   }
 }
