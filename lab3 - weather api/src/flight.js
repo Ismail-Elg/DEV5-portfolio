5f498101c6d2ddb3a4d2686bb7c39753c9cea6f0
Ismail El Ghamri <elghamri.ismail@gmail.com>
2022-10-05 13:59:35 +0200

    feat: added flight class

---
+++ lab3 - weather api/src/flight.js
@@ -0,0 +1,26 @@
+class Flight {
+ 
+    constructor(where) {
+        this.where = where;
+    }
+
+ 
+    async getFlight() {
+
+        const response = await fetch(`https://travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com/v2/prices/latest?destination=MAD&origin=BRU&period_type=year&one_way=%20&show_to_affiliates=true&trip_class=0&currency=EUR&page=1&sorting=price&limit=1`, {
+            method: 'GET',
+	headers: {
+		'X-Access-Token': '9bb7a1b1eba1507b24e2c69c9509ddc2',
+		'X-RapidAPI-Key': '85af616e74msh8565637bcff0afep1e7d54jsnb711aafc1cb1',
+		'X-RapidAPI-Host': 'travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com'
+	}
+        });
+
+        const responseData = await response.json();
+        console.log(responseData);
+        return responseData;
+
+       
+    }
+}
+export default Flight;
