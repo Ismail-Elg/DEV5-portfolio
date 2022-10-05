15fb85df60557fcc877940634acb693a10734c33
Ismail El Ghamri <elghamri.ismail@gmail.com>
2022-10-05 13:59:15 +0200

    feat: connection with pexels api to return background images

--- lab3 - weather api/src/pexel.js
+++ lab3 - weather api/src/pexel.js
@@ -1,22 +1,15 @@
 class Pexel {
- 
     constructor(type) {
         this.type = type;
     }
-
- 
     async getCity() {
-
         const response = await fetch(`https://api.pexels.com/v1/search?query=${this.type}`, {
             "headers": {
-                'Authorization':  '563492ad6f917000010000013ac6b1670442418e90619b903057d604',
+                'Authorization': '563492ad6f917000010000013ac6b1670442418e90619b903057d604',
             },
         });
-
         const responseData = await response.json();
         return responseData;
-
-       
     }
 }
 export default Pexel;
