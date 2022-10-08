5a531dda231ae3563269a411889ea593ca6f4958
Ismail <El Ghamri>
2022-10-08 13:43:11 +0200

    feat: created scene class

---
+++ lab4 - threejs donut/src/js/index.js
@@ -0,0 +1,46 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import Stats from "stats.js";
+import { gsap } from "gsap";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+
+
+class Scene {
+    constructor() {
+      
+    }
+
+    init() {
+   
+    }
+
+   
+    createMesh() {
+
+    }
+
+    createLights() {
+
+    }
+
+    addEvents() {
+      
+    }
+    run() {
+    
+    }
+
+    render() {
+       
+    }
+
+    onResize() {
+    
+    }
+
+    helpers() {
+    
+    }
+}
+
+export default Scene;
