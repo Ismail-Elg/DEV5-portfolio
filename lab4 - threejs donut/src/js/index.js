bee83b26a992f1a86d8453b7abbb9cdc9b626884
Ismail <El Ghamri>
2022-10-08 13:58:09 +0200

    feat: created scene constructor

--- lab4 - threejs donut/src/js/index.js
+++ lab4 - threejs donut/src/js/index.js
@@ -7,11 +7,29 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 class Scene {
     constructor() {
-      
+        this.scene = new THREE.Scene();
+        this.camera = new THREE.PerspectiveCamera(
+            75,
+            window.innerWidth / window.innerHeight,
+            0.1,
+            1000
+        );
+
+        this.camera.position.set(0, 0, 1.5);
+        this.camera.lookAt(0, 0, 0);
+
+        this.renderer = new THREE.WebGLRenderer({
+            canvas: document.querySelector(".webgl"),
+            antialias: true,
+        });
+
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+
+
     }
 
     init() {
-   
+        
     }
 
    
