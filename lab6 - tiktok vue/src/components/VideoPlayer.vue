94de6c26880f0c774b9cd68c7bec9ecc3b04838e
Ismail <elghamri.ismail@gmail.com>
2022-11-21 18:44:57 +0100

    style: added styling for video

--- lab6 - tiktok vue/src/components/VideoPlayer.vue
+++ lab6 - tiktok vue/src/components/VideoPlayer.vue
@@ -1,5 +1,5 @@
 <script setup>
- 
+
 </script>
 
 <template>
@@ -17,5 +17,44 @@
 
 <style scoped>
 
+.video-container{
+    background-color: black;
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    padding: 10%;
+    position: relative;
+}
+.video-container video{
+    
+    height: 100%;
+    object-fit: cover;
+
+}
+.controls{
+    position: absolute;
+   
+    top: 50%;
+    right: 0%;
+    width: 40px;
+    height: 40px;
+    background-color: gray;
+    transform: translate(-50%, -50%);
+    z-index: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50%;
+    cursor: pointer;
+
+
+}
+.controls a{
+    text-decoration: none;
+    color: white;
+    font-size: 2rem;
+   
+}
 </style>
 
