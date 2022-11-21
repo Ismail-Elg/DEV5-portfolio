242894c6af6c4519bd80ad2b9b9f95fe38f8f085
Ismail <elghamri.ismail@gmail.com>
2022-11-21 18:44:10 +0100

    feat: added videoplayer template

--- lab6 - tiktok vue/src/App.vue
+++ lab6 - tiktok vue/src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
-
+import VideoPlayer from './components/VideoPlayer.vue';
 </script>
 
 <template>
-
+<div class="container">
+  <div class="video"> <video-player></video-player></div>
+</div>
 </template>
 
 <style scoped>
---
+++ lab6 - tiktok vue/src/components/VideoPlayer.vue
@@ -0,0 +1,21 @@
+<script setup>
+ 
+</script>
+
+<template>
+   
+  <div class="video-container">
+    <div class="controls">
+    <a @click.prevent="nextVideo" href="#" class="controls__next">⤸</a>    
+    </div>
+    <video :src="src" autoplay loop muted controls class="u--slideUp" :class="animation">
+        Your browser does not support the video tag.
+    </video>
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
+
