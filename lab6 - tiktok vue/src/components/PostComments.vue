c091d9dc657cd90d087244513b781481271c6374
Ismail <elghamri.ismail@gmail.com>
2022-11-21 18:55:27 +0100

    feat: setup template for comments component

---
+++ lab6 - tiktok vue/src/components/PostComments.vue
@@ -0,0 +1,29 @@
+<script setup>
+ import { ref, onMounted } from 'vue';
+
+
+
+    onMounted(() => {
+        let api_url = "https://lab5-p379.onrender.com/api/v1/messages";
+        fetch(api_url)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+             
+
+            });
+
+      
+   });
+</script>
+
+<template>
+    <ul>
+        <li>{{user}}</li>
+        <li>{{text}}</li>
+    </ul>
+</template>
+
+<style scoped>
+  
+</style>
