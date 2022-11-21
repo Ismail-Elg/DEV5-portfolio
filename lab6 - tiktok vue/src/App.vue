9e3cabd0db26e6a7e948931c0f69415210350f92
Ismail <elghamri.ismail@gmail.com>
2022-11-21 18:48:52 +0100

    style: added grid

--- lab6 - tiktok vue/src/App.vue
+++ lab6 - tiktok vue/src/App.vue
@@ -1,12 +1,26 @@
 <script setup>
 import VideoPlayer from './components/VideoPlayer.vue';
+import PostDetails from './components/PostDetails.vue';
 </script>
 
 <template>
 <div class="container">
   <div class="video"> <video-player></video-player></div>
+  <div class="title"> <post-details></post-details></div>
 </div>
 </template>
 
 <style scoped>
+.container {  display: grid;
+  grid-template-columns: 8fr 4fr;
+  grid-template-rows: 1fr 4fr;
+  gap: 0px 0px;
+  grid-auto-flow: row;
+  grid-template-areas:
+    "video title"
+    "video comments";
+}
+.video { grid-area: video; }
+
+.title { grid-area: title;  padding: 10%; border-bottom: 1px solid rgba(128, 128, 128, 0.35);}
 </style>
