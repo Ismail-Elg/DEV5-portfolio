6d26366b11023d2ec214626d8af7ac2d975c1e74
Ismail <elghamri.ismail@gmail.com>
2022-11-21 18:56:01 +0100

    feat: added comments to app

--- lab6 - tiktok vue/src/App.vue
+++ lab6 - tiktok vue/src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
 import VideoPlayer from './components/VideoPlayer.vue';
 import PostDetails from './components/PostDetails.vue';
+import PostComments from './components/PostComments.vue';
 </script>
 
 <template>
 <div class="container">
   <div class="video"> <video-player></video-player></div>
   <div class="title"> <post-details></post-details></div>
+  <div class="comments"><post-comments></post-comments></div>
 </div>
 </template>
 
@@ -23,4 +25,6 @@ import PostDetails from './components/PostDetails.vue';
 .video { grid-area: video; }
 
 .title { grid-area: title;  padding: 10%; border-bottom: 1px solid rgba(128, 128, 128, 0.35);}
+
+.comments { grid-area: comments; padding: 10%; }
 </style>
