b025f8d1e8eec7f8f4a1c0c265eb1e65aabc52d4
Ismail <elghamri.ismail@gmail.com>
2022-11-21 18:48:34 +0100

    feat: print post details

---
+++ lab6 - tiktok vue/src/components/PostDetails.vue
@@ -0,0 +1,27 @@
+<script setup>
+ import { ref, onMounted } from 'vue';
+
+ let username = ref('');
+ let description = ref('');
+
+    onMounted(() => {
+      // let api_url = "https://app.fakejson.com/q/Dt6soyE2?token=J8LA1-xpV4FBoiU5vS0n4Q";
+      let api_url = "http://127.0.0.1:5173/tiktok.json";
+        fetch(api_url)
+            .then(response => response.json())
+            .then(data => {
+                username.value = data.videos[0].username;
+                description.value = data.videos[0].description;
+        
+            });
+
+      
+   });
+</script>
+
+<template>
+  <div class="post-details">
+    <h3>{{username}}</h3>
+    <p>{{description}}</p>
+   </div>
+</template>
